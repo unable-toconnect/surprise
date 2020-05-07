ad618a74c481d100b74ec6cd9334642c8c1cb403
pradeep <50012011@acsind.com>
2020-05-07 13:33:57 +0530

    updated

--- db.java
+++ db.java
@@ -1,8 +1,1 @@
-
-hi this is my first work
-
-master branch added
-dev added
-lets see how much i am gaining from git
-i dont thing that much but people are making me
-hope it will work this time 
+i am the first user
